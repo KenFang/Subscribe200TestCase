610d66d7356732372b67f3693574e0d7d3b7dbd8
KenFang <kenfangch@yahoo.com.tw>
2022-05-04 15:10:58 +0800

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,18 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+// Let's start simple: We always return a 200 Ok
+async fn subuscribe()-> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // Return 'Server' on the happy path
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(||  {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subuscribe))
     })
         .listen(listener)?
         .run();
